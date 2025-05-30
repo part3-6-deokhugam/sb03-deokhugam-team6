e74b07360457c213949f80642fe33026c2fe26bf
chanhyeong0403 <parkchanhyeong0403@gmail.com>
2025-05-30 09:24:24 +0900

    init: Spring Boot Initialization

---
+++ src/test/java/com/part3/deokhugam/deokhugam/DeokhugamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.part3.deokhugam.deokhugam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeokhugamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
