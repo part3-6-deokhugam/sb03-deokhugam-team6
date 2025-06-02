e723a637d9978529518c15aa3260c2753b684d8e
hyorim <tg305@naver.com>
2025-06-02 17:35:43 +0900

    feat: Comment 등록 기능 구현 (Service, Mapper, Repository 구성)

--- src/main/java/com/part3/deokhugam/service/CommentService.java
+++ src/main/java/com/part3/deokhugam/service/CommentService.java
@@ -1,4 +1,34 @@
 package com.part3.deokhugam.service;
 
+import com.part3.deokhugam.domain.Comment;
+import com.part3.deokhugam.domain.Review;
+import com.part3.deokhugam.domain.User;
+import com.part3.deokhugam.dto.comment.CommentCreateRequest;
+import com.part3.deokhugam.dto.comment.CommentDto;
+import com.part3.deokhugam.mapper.CommentMapper;
+import com.part3.deokhugam.repository.CommentRepository;
+import com.part3.deokhugam.repository.ReviewRepository;
+import com.part3.deokhugam.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CommentService {
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final ReviewRepository reviewRepository;
+    private final CommentMapper commentMapper;
+
+    public CommentDto create(CommentCreateRequest request) {
+        User user = userRepository.findById(request.getUserId())
+                .orElseThrow(() -> new RuntimeException("User Not Found"));
+        Review review = reviewRepository.findById(request.getReviewId())
+                .orElseThrow(() -> new RuntimeException("Review Not Found"));
+
+        Comment comment = commentMapper.toEntity(request, user, review);
+        Comment savedComment = commentRepository.save(comment);
+
+        return commentMapper.toDto(savedComment);
+    }
 }
