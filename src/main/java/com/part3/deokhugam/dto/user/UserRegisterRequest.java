923c499110e615ec01d912cc7342709fc84156af
Jaeseok Heo <jasonheo1998@gmail.com>
2025-06-02 10:14:22 +0900

    feat: UserRegisterRequest 구현(회원가입 요청 데이터)

--- src/main/java/com/part3/deokhugam/dto/user/UserRegisterRequest.java
+++ src/main/java/com/part3/deokhugam/dto/user/UserRegisterRequest.java
@@ -1,4 +1,27 @@
 package com.part3.deokhugam.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserRegisterRequest {
+  @Email
+  @NotBlank
+  private String email;
+
+  @NotBlank @Size(min = 8, max = 20)
+  private String password;
+
+  @NotBlank @Size(min = 2, max = 20)
+  private String nickname;
 }
