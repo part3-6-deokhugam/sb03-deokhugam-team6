7d0758a6a1e5d2ecd5e6eb4ccf1553b69a47ff57
chanhyeong0403 <parkchanhyeong0403@gmail.com>
2025-06-02 17:59:06 +0900

    feat: PopularBookScheduler 추가

---
+++ src/main/java/com/part3/deokhugam/service/PopularBookScheduler.java
@@ -0,0 +1,22 @@
+package com.part3.deokhugam.service;
+
+import com.part3.deokhugam.domain.enums.Period;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class PopularBookScheduler {
+    private final BookService bookService;
+
+    // 매일 자정 배치 연산 수행
+    @Scheduled(cron = "${scheduler.batch--start-time}")
+    public void updateRanking() {
+        for (Period period : Period.values()) {
+            bookService.calculateRanking(period);
+        }
+    }
+}
