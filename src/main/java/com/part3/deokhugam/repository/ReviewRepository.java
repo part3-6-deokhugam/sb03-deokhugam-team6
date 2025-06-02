4e1ffb007964c382d9a35fcaa221d97ca594fddf
chanhyeong0403 <parkchanhyeong0403@gmail.com>
2025-06-02 17:29:09 +0900

    feat: ReviewRepository 추가

--- src/main/java/com/part3/deokhugam/repository/ReviewRepository.java
+++ src/main/java/com/part3/deokhugam/repository/ReviewRepository.java
@@ -3,9 +3,14 @@ package com.part3.deokhugam.repository;
 import com.part3.deokhugam.domain.Review;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ReviewRepository  extends JpaRepository<Review, UUID> {
-  boolean existsByBookIdAndUserIdAndDeletedFalse(UUID bookId, UUID userId);
+import java.time.Instant;
+import java.util.List;
+
+public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom {
+    List<Review> findByCreatedAtBetween(Instant start, Instant end);
+
+    List<Review> findByCreatedAtBetweenAndDeletedFalse(Instant start, Instant end);
+
+    boolean existsByBookIdAndUserIdAndDeletedFalse(UUID bookId, UUID userId);
 }
