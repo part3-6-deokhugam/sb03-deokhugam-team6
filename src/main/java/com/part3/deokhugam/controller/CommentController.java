d339940c858e42a596615aa04eba47fc3a0f8083
hyorim <tg305@naver.com>
2025-06-02 18:37:58 +0900

    feat: Comment 등록 컨트롤러 및 API 명세 구현

--- src/main/java/com/part3/deokhugam/controller/CommentController.java
+++ src/main/java/com/part3/deokhugam/controller/CommentController.java
@@ -1,4 +1,28 @@
 package com.part3.deokhugam.controller;
 
-public class CommentController {
+import com.part3.deokhugam.api.CommentApi;
+import com.part3.deokhugam.dto.comment.CommentCreateRequest;
+import com.part3.deokhugam.dto.comment.CommentDto;
+import com.part3.deokhugam.service.CommentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/comments")
+public class CommentController implements CommentApi {
+    private final CommentService commentService;
+
+    @Override
+    @PostMapping
+    public ResponseEntity<CommentDto> create(@RequestBody @Valid CommentCreateRequest request) {
+        CommentDto commentDto = commentService.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(commentDto);
+    }
 }
