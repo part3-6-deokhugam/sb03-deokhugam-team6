e9302595ad163090e16aa9df6517fb0b65d2edf2
Jaeseok Heo <jasonheo1998@gmail.com>
2025-06-02 10:20:29 +0900

    feat: UserUpdateRequest 구현(사용자 정보 수정 요청 데이터)

--- src/main/java/com/part3/deokhugam/dto/user/UserUpdateRequest.java
+++ src/main/java/com/part3/deokhugam/dto/user/UserUpdateRequest.java
@@ -1,4 +1,19 @@
 package com.part3.deokhugam.dto.user;
 
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserUpdateRequest {
+  @NotBlank @Size(min = 2, max = 20)
+  private String nickname;
 }
