c37ee906321f71e2e54dbe1100c1665d51e46ca5
chanhyeong0403 <parkchanhyeong0403@gmail.com>
2025-06-02 16:20:54 +0900

    feat: BookRepository 추가

--- src/main/java/com/part3/deokhugam/repository/BookRepository.java
+++ src/main/java/com/part3/deokhugam/repository/BookRepository.java
@@ -1,4 +1,15 @@
 package com.part3.deokhugam.repository;
 
-public class BookRepository {
+import com.part3.deokhugam.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface BookRepository extends JpaRepository<Book, UUID>, BookRepositoryCustom {
+
+    boolean existsByIsbn(String isbn);
+
+    Optional<Book> findByIdAndDeletedFalse(UUID bookId);
 }
