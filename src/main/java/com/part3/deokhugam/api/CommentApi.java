1d9e4a798c6fe18a3cd99df7ae63d389fd1df505
hyorim <tg305@naver.com>
2025-06-03 16:54:59 +0900

    chore: Comment 수정 및 삭제 API 구현

--- src/main/java/com/part3/deokhugam/api/CommentApi.java
+++ src/main/java/com/part3/deokhugam/api/CommentApi.java
@@ -2,6 +2,7 @@ package com.part3.deokhugam.api;
 
 import com.part3.deokhugam.dto.comment.CommentCreateRequest;
 import com.part3.deokhugam.dto.comment.CommentDto;
+import com.part3.deokhugam.dto.comment.CommentUpdateRequest;
 import com.part3.deokhugam.exception.ErrorResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -9,8 +10,14 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.UUID;
 
 @Tag(name = "댓글 관리", description = "댓글 관련 API")
 @RequestMapping("/api/comments")
@@ -29,5 +36,57 @@ public interface CommentApi {
             @ApiResponse(responseCode = "500", description = "서버 내부 오류",
                     content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
-    ResponseEntity<CommentDto> create(CommentCreateRequest request);
+    ResponseEntity<CommentDto> create(@Valid @RequestBody CommentCreateRequest request);
+
+    @Operation(
+            summary = "댓글 수정",
+            description = "본인이 작성한 댓글을 수정합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "댓글 수정 성공",
+                    content = @Content(schema = @Schema(implementation = CommentDto.class))),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청(입력값 검증 실패,요청자 ID 누락)",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "403", description = "댓글 수정 권한 없음",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "댓글 정보 없음",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    ResponseEntity<CommentDto> update(
+            UUID commentId,
+            UUID userId,
+            CommentUpdateRequest request);
+
+    @Operation(
+            summary = "댓글 논리 삭제",
+            description = "본인이 작성한 댓글을 논리적으로 삭제합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "댓글 삭제 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청 (요청자 ID 누락)", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "403", description = "댓글 삭제 권한 없음", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "댓글 정보 없음", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    ResponseEntity<Void> deleteLogically(
+            @PathVariable UUID commentId,
+            @RequestHeader("Deokhugam-Request-User-ID") UUID userId
+    );
+    @Operation(
+            summary = "댓글 물리 삭제",
+            description = "본인이 작성한 댓글을 물리적으로 삭제합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "댓글 삭제 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청 (요청자 ID 누락)", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "403", description = "댓글 삭제 권한 없음", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "댓글 정보 없음", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    ResponseEntity<Void> deletePhysically(
+            @PathVariable UUID commentId,
+            @RequestHeader("Deokhugam-Request-User-ID") UUID userId
+    );
 }
--- src/main/java/com/part3/deokhugam/controller/CommentController.java
+++ src/main/java/com/part3/deokhugam/controller/CommentController.java
@@ -3,15 +3,15 @@ package com.part3.deokhugam.controller;
 import com.part3.deokhugam.api.CommentApi;
 import com.part3.deokhugam.dto.comment.CommentCreateRequest;
 import com.part3.deokhugam.dto.comment.CommentDto;
+import com.part3.deokhugam.dto.comment.CommentUpdateRequest;
 import com.part3.deokhugam.service.CommentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,5 +24,34 @@ public class CommentController implements CommentApi {
     public ResponseEntity<CommentDto> create(@RequestBody @Valid CommentCreateRequest request) {
         CommentDto commentDto = commentService.create(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(commentDto);
+    }
+
+    @Override
+    @PatchMapping("/{commentId}")
+    public ResponseEntity<CommentDto> update(
+            @PathVariable UUID commentId,
+            @RequestHeader("Deokhugam-Request-User-ID") UUID userId,
+            @RequestBody @Valid CommentUpdateRequest request) {
+
+        CommentDto updated = commentService.update(commentId, userId, request);
+        return ResponseEntity.status(HttpStatus.OK).body(updated);
+    }
+
+    @DeleteMapping("/{commentId}")
+    @Override
+    public ResponseEntity<Void> deleteLogically(
+            @PathVariable UUID commentId,
+            @RequestHeader("Deokhugam-Request-User-ID") UUID userId) {
+        commentService.deleteLogically(commentId, userId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{commentId}/hard")
+    @Override
+    public ResponseEntity<Void> deletePhysically(
+            @PathVariable UUID commentId,
+            @RequestHeader("Deokhugam-Request-User-ID") UUID userId) {
+        commentService.deletePhysically(commentId, userId);
+        return ResponseEntity.noContent().build(); // 204
     }
 }
