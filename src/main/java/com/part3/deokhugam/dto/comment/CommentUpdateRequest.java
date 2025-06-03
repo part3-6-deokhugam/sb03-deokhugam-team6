8ab979100bf9094bd3e39615fc69dac994ca8fde
hyorim <tg305@naver.com>
2025-06-03 11:38:10 +0900

    feat: Comment 수정 기능 구현 (DTO + 서비스 메서드)

--- src/main/java/com/part3/deokhugam/dto/comment/CommentUpdateRequest.java
+++ src/main/java/com/part3/deokhugam/dto/comment/CommentUpdateRequest.java
@@ -1,4 +1,14 @@
 package com.part3.deokhugam.dto.comment;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentUpdateRequest {
+    @NotBlank
+    private String content;
 }
--- src/main/java/com/part3/deokhugam/service/CommentService.java
+++ src/main/java/com/part3/deokhugam/service/CommentService.java
@@ -5,12 +5,16 @@ import com.part3.deokhugam.domain.Review;
 import com.part3.deokhugam.domain.User;
 import com.part3.deokhugam.dto.comment.CommentCreateRequest;
 import com.part3.deokhugam.dto.comment.CommentDto;
+import com.part3.deokhugam.dto.comment.CommentUpdateRequest;
 import com.part3.deokhugam.mapper.CommentMapper;
 import com.part3.deokhugam.repository.CommentRepository;
 import com.part3.deokhugam.repository.ReviewRepository;
 import com.part3.deokhugam.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +24,7 @@ public class CommentService {
     private final ReviewRepository reviewRepository;
     private final CommentMapper commentMapper;
 
+    @Transactional
     public CommentDto create(CommentCreateRequest request) {
         User user = userRepository.findById(request.getUserId())
                 .orElseThrow(() -> new RuntimeException("User Not Found"));
@@ -30,5 +35,16 @@ public class CommentService {
         Comment savedComment = commentRepository.save(comment);
 
         return commentMapper.toDto(savedComment);
+    }
+    @Transactional
+    public CommentDto update(UUID commentId, UUID userId, CommentUpdateRequest request) {
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(()-> new RuntimeException("Comment Not Found"));
+        if(!comment.getUser().getId().equals(userId)) {
+            throw new RuntimeException("댓글 작성자가 아닙니다");
+        }
+        comment.update(request.getContent());
+        return commentMapper.toDto(comment);
+
     }
 }
