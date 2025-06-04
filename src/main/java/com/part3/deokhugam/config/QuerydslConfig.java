38e74cc45c6aa90d68d7ae323b38674200628d17
Jaeseok Heo <jasonheo1998@gmail.com>
2025-06-04 18:10:03 +0900

    feat: QuerydslConfig 구현

--- src/main/java/com/part3/deokhugam/config/QuerydslConfig.java
+++ src/main/java/com/part3/deokhugam/config/QuerydslConfig.java
@@ -10,8 +10,14 @@ import org.springframework.context.annotation.Configuration;
 public class QuerydslConfig {
 
   @PersistenceContext
+  private EntityManager em;
 
+  /**
+   * BookRepositoryImpl 생성자 주입을 위해 JPAQueryFactory 빈만 등록.
+   * (실제로 QClass가 없어도, 빈이 존재하기만 하면 스프링 컨텍스트가 시작됨)
+   */
   @Bean
   public JPAQueryFactory jpaQueryFactory() {
+    return new JPAQueryFactory(em);
   }
 }
