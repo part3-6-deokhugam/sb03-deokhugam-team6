b222f898fc9877c30cf442d237fe046edd70ff38
최성현 <round1126@naver.com>
2025-06-04 18:21:37 +0900

    feat: review 관련 dto 추가

--- src/main/java/com/part3/deokhugam/dto/review/ReviewLikeDto.java
+++ src/main/java/com/part3/deokhugam/dto/review/ReviewLikeDto.java
@@ -1,4 +1,20 @@
 package com.part3.deokhugam.dto.review;
 
+import jakarta.validation.constraints.NotNull;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ReviewLikeDto {
+  private UUID reviewId;
+  private UUID userId;
+  private Boolean Liked;
 }
