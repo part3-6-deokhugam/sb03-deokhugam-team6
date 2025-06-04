c1198fda619d3c9261c4969ba8a3e1db63bcd56a
chanhyeong0403 <parkchanhyeong0403@gmail.com>
2025-06-04 13:06:00 +0900

    fix: 필요없는 메서드 제거

--- src/main/java/com/part3/deokhugam/repository/PopularBookRepository.java
+++ src/main/java/com/part3/deokhugam/repository/PopularBookRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface PopularBookRepository extends JpaRepository<PopularBook, UUID>, PopularBookRepositoryCustom {
-    String period(Period period);
 }
