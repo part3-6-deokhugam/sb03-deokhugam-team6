aac1b4ddb0fea853d122ee3d9883fe8c059a0322
Jaeseok Heo <jasonheo1998@gmail.com>
2025-06-05 10:38:15 +0900

    feat: UserRankData 엔티티, 레포지토리 구현

---
+++ src/main/java/com/part3/deokhugam/repository/UserRankDataRepository.java
@@ -0,0 +1,16 @@
+package com.part3.deokhugam.repository;
+
+import com.part3.deokhugam.domain.UserRankData;
+import com.part3.deokhugam.domain.enums.Period;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRankDataRepository extends JpaRepository<UserRankData, UUID> {
+  List<UserRankData> findByPeriodAndPeriodDateOrderByRankAsc(
+      Period period,
+      LocalDate periodDate);
+}
