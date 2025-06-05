c07bb44e3a2c8de69703bc2502154a959e84d672
chanhyeong0403 <parkchanhyeong0403@gmail.com>
2025-06-05 11:18:42 +0900

    fix: 스케쥴러 환경변수 올바르게 주입하도록 수정

--- src/main/java/com/part3/deokhugam/batch/PopularBookScheduler.java
+++ src/main/java/com/part3/deokhugam/batch/PopularBookScheduler.java
@@ -1,6 +1,7 @@
-package com.part3.deokhugam.service;
+package com.part3.deokhugam.batch;
 
 import com.part3.deokhugam.domain.enums.Period;
+import com.part3.deokhugam.service.BookService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,7 +14,7 @@ public class PopularBookScheduler {
     private final BookService bookService;
 
     // 매일 자정 배치 연산 수행
-    @Scheduled(cron = "${scheduler.batch--start-time}")
+    @Scheduled(cron = "${scheduler.batch.start-time}")
     public void updateRanking() {
         for (Period period : Period.values()) {
             bookService.calculateRanking(period);
