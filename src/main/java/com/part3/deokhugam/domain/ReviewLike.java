e9c177bce916ef768fd2e277bd9ba10e724da065
최성현 <round1126@naver.com>
2025-06-05 15:14:16 +0900

    feat: reviewLike 엔티티 구현

--- src/main/java/com/part3/deokhugam/domain/ReviewLike.java
+++ src/main/java/com/part3/deokhugam/domain/ReviewLike.java
@@ -1,5 +1,33 @@
 package com.part3.deokhugam.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "review_like")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewLike {
 
+  @EmbeddedId
+  private ReviewLikeId id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @MapsId("reviewId")
+  @JoinColumn(name = "review_id", nullable = false)
+  private Review review;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @MapsId("userId")
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @Column(nullable = false)
+  private boolean liked = false;
+
+  @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMPTZ DEFAULT now()")
+  private ZonedDateTime createdAt = ZonedDateTime.now();
 }
