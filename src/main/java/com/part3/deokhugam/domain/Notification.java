0a38eb656b4193fa8a34f34a2a5dac64775669ae
Jaeseok Heo <jasonheo1998@gmail.com>
2025-06-09 13:16:54 +0900

    feat: Notification DTO 구성

--- src/main/java/com/part3/deokhugam/domain/Notification.java
+++ src/main/java/com/part3/deokhugam/domain/Notification.java
@@ -1,4 +1,51 @@
 package com.part3.deokhugam.domain;
 
-public class Notification {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "notification")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Notification extends BaseEntity {
+
+  @Id
+  @GeneratedValue
+  private UUID id;
+
+  @Column(name = "content", nullable = false)
+  private String content;
+
+  @Column(name = "confirmed", nullable = false)
+  private boolean confirmed = false;
+
+  // 작성자
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  // 알림 대상 리뷰
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "review_id", nullable = false)
+  private Review review;
+
+  // (Optional) 댓글 알림이라면
+  @Column(name = "comment_id")
+  private UUID commentId;
+
 }
--- src/main/java/com/part3/deokhugam/dto/notification/NotificationUpdateRequest.java
+++ src/main/java/com/part3/deokhugam/dto/notification/NotificationUpdateRequest.java
@@ -1,4 +1,8 @@
 package com.part3.deokhugam.dto.notification;
 
+import lombok.Getter;
+
+@Getter
 public class NotificationUpdateRequest {
+  private boolean confirmed;
 }
