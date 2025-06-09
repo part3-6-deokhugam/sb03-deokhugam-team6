1f0fb4b58cb23340b3058e9fdcb2fce24eb5a218
Jaeseok Heo <jasonheo1998@gmail.com>
2025-06-09 13:23:19 +0900

    feat: NotificationRepo 인터페이스 구현

--- src/main/java/com/part3/deokhugam/repository/NotificationRepository.java
+++ src/main/java/com/part3/deokhugam/repository/NotificationRepository.java
@@ -1,4 +1,8 @@
 package com.part3.deokhugam.repository;
 
-public class NotificationRepository {
+import com.part3.deokhugam.domain.Notification;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {
 }
