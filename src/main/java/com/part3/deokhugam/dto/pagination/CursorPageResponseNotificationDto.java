445024749e46774e9d9afbdfbafec3c3b7d18e23
Jaeseok Heo <jasonheo1998@gmail.com>
2025-06-09 15:23:09 +0900

    feat: CursorPageResponseNotificationDto 구현

--- src/main/java/com/part3/deokhugam/dto/pagination/CursorPageResponseNotificationDto.java
+++ src/main/java/com/part3/deokhugam/dto/pagination/CursorPageResponseNotificationDto.java
@@ -1,4 +1,16 @@
 package com.part3.deokhugam.dto.pagination;
 
+import com.part3.deokhugam.dto.notification.NotificationDto;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.Instant;
+import java.util.List;
+
+@Getter
+@Builder
 public class CursorPageResponseNotificationDto {
+  private List<NotificationDto> data;
+  private boolean hasNext;
+  private Instant nextCursor;   // 다음 페이지를 불러올 커서(=마지막 항목의 createdAt)
 }
