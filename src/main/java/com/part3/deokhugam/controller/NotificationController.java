e26a82190afaf3a8e844984e28aa7f275452ffe8
Jaeseok Heo <jasonheo1998@gmail.com>
2025-06-10 11:09:28 +0900

    fix: RequestMapping 중복 문제

--- src/main/java/com/part3/deokhugam/controller/NotificationController.java
+++ src/main/java/com/part3/deokhugam/controller/NotificationController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/notifications")
 @RequiredArgsConstructor
 public class NotificationController implements NotificationApi {
   private final NotificationService notificationService;
