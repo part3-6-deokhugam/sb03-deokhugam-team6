bebae1d4c3125d44121506765f02c6f7e855cd5d
Jaeseok Heo <jasonheo1998@gmail.com>
2025-06-10 10:49:49 +0900

    fix: 요청 헤더에 안들어가는 문제

--- src/main/java/com/part3/deokhugam/dto/user/UserLoginResponse.java
+++ src/main/java/com/part3/deokhugam/dto/user/UserLoginResponse.java
@@ -1,10 +1,14 @@
 package com.part3.deokhugam.dto.user;
 
+import java.time.Instant;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class UserLoginResponse {
-  private String userId;
+  private String id;
+  private String email;
+  private String nickname;
+  private Instant createdAt;
 }
