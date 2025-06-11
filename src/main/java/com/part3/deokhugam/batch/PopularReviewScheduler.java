e5e6d72c831a7f9b43d04568aee4aa1c35b78dde
최성현 <round1126@naver.com>
2025-06-11 18:35:18 +0900

    feat: popularReview 스케쥴러 구현

---
+++ src/main/java/com/part3/deokhugam/batch/PopularReviewScheduler.java
@@ -0,0 +1,22 @@
+package com.part3.deokhugam.batch;
+
+import com.part3.deokhugam.domain.enums.Period;
+import com.part3.deokhugam.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class PopularReviewScheduler {
+  private final ReviewService reviewService;
+
+  @Scheduled(cron ="0 0 0 * * *")
+  public void updatePopularReviews() {
+    for(Period period : Period.values()) {
+      reviewService.calculateReview(period);
+    }
+  }
+}
