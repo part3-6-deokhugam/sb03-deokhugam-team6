1f16cbb23c5fac6a76b4983ebb94824ef7f04f1c
Jaeseok Heo <jasonheo1998@gmail.com>
2025-06-14 23:05:47 +0900

    fix : 새로 고침 문제 해결

--- src/main/java/com/part3/deokhugam/controller/HomeController.java
+++ src/main/java/com/part3/deokhugam/controller/HomeController.java
@@ -1,13 +1,13 @@
-package com.part3.deokhugam.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class HomeController {
-
-    @GetMapping("/")
-    public String home() {
-        return "index";
-    }
-}
+//package com.part3.deokhugam.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class HomeController {
+//
+//    @GetMapping("/")
+//    public String home() {
+//        return "index";
+//    }
+//}
