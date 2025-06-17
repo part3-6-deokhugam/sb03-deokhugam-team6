852525c1324d39170b2b56d9d8dc3ba5c6a09147
chanhyeong0403 <parkchanhyeong0403@gmail.com>
2025-06-17 09:47:23 +0900

    fix: PopularBookMapper DTO 매핑 수정

--- src/main/java/com/part3/deokhugam/mapper/PopularBookMapper.java
+++ src/main/java/com/part3/deokhugam/mapper/PopularBookMapper.java
@@ -3,8 +3,14 @@ package com.part3.deokhugam.mapper;
 import com.part3.deokhugam.domain.PopularBook;
 import com.part3.deokhugam.dto.book.PopularBookDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PopularBookMapper {
+    @Mapping(source = "book.id", target = "bookId")
+    @Mapping(source = "book.title", target = "title")
+    @Mapping(source = "book.author", target = "author")
+    @Mapping(source = "book.thumbnailUrl", target = "thumbnailUrl")
+    @Mapping(source = "book.bookMetrics.averageRating", target = "rating")
     PopularBookDto toDto(PopularBook popularBook);
 }
